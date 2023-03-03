0d3bcb8304995a8946a011f092e754f2d1bed36b
Mai <Mai@example.com~>
2023-03-03 15:41:21 +0700

    del

---
+++ Maine/Areas/Admin/Models/LoginModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Maine.Areas.Admin.Models
+{
+    public class LoginModel
+    {
+        internal string UserName;
+
+        [Required(ErrorMessage ="Mời nhập user name")]
+        public string LoginProvider { get; set; }
+
+        [Required(ErrorMessage = "Mời nhập password")]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ Model/Dao/UserDao.cs
@@ -0,0 +1,56 @@
+﻿using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Dao
+{
+    public class UserDao
+    {
+        MaineDbContext db = null;
+        public UserDao()
+        {
+            db = new MaineDbContext();
+        }
+
+        public long Insert(User entity)
+        {
+            db.User.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+        public User GetByUserName(string userName) 
+        {
+            return db.User.SingleOrDefault(x => x.UserName == userName);
+        }
+        public object Login(string userName, string passWord)
+        {
+            var result = db.User.SingleOrDefault(x => x.UserName == userName);
+            if (result == null)
+            {
+                return 0;
+            }
+            else
+            {
+                if(result.Status == false) 
+                {
+                    return -1;
+                }
+                else 
+                {
+                    if (result.Password == passWord)
+                        return 1;
+                    else
+                        return -2;
+                }
+            }
+        }
+
+        
+        }
+    }
+
+
+        
